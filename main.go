11755613d66793c48904da62a56352f2bb081bcb
asilnikov <51442050+asilnikov@users.noreply.github.com>
2020-03-19 16:24:44 +0300

    test

--- main.go
+++ main.go
@@ -1,17 +1,1 @@
-package main
 
-import "testing"
-
-func Equal(t *testing.T, actual interface{}, expected interface{}, message string) {
-        assert(t, actual, expected, message, true)
-}
-
-func NotEqual(t *testing.T, actual interface{}, expected interface{}, message string) {
-        assert(t, actual, expected, message, false)
-}
-
-func assert(t *testing.T, actual interface{}, expected interface{}, message string, expectation bool) {
-        if (expected == actual) != expectation {
-                t.Errorf("%s failed: Expected %v, but got %v", message, expected, actual)
-        }
-}
